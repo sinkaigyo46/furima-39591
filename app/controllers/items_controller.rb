b6777b80fe607c8d5a024c442aaf4a2a403c391e
sinkaigyo46 <suguru.1096@yahoo.ne.jp>
2023-08-01 16:52:06 +0900

    ログインの有無で遷移先を指定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,32 @@
 class ItemsController < ApplicationController
+  before_action :move_to_signed_in, only: :new
   def index
+  end
+  def new
+    @item = Item.new
+  end
+  def create
+   @item = Item.new(item_params)
+   if @item.save
+    redirect_to items_path
+  else
+    render :new, status: :unprocessable_entity
+  end
+  end
+  
+
+
+
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :image, :content, :category_id, :status_id, :prefecture_id, :cost_bearer_id,:shipping_day_id, :price).merge(user_id: current_user.id)
+  end
+
+  def move_to_signed_in
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
   end
 end
