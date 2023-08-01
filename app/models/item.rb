a70ba85dbee1ec10888b1cf1949b4d616a2e7040
sinkaigyo46 <suguru.1096@yahoo.ne.jp>
2023-08-01 18:27:22 +0900

    itemモデルのテスト

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,6 +10,6 @@ class Item < ApplicationRecord
 
   validates :name, :content, :user,:image,:price, presence: true
   validates :category_id, :status_id, :prefecture_id, :cost_bearer_id, :shipping_day_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :price, numericality: { greater_than_or_equal_to:300, less_than_or_equal_to:9999999,message: "Out of setting range"}
+  validates :price, numericality: { greater_than_or_equal_to:300, less_than_or_equal_to:9999999,message: "out of setting range"}
   validates :price, numericality: { only_integer: true, message: "Half-width number." }
 end
