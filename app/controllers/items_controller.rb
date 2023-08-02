a9147b94f357f13df2419ee5740ce7b7f4530eb2
sinkaigyo46 <suguru.1096@yahoo.ne.jp>
2023-08-02 20:11:11 +0900

    編集ページへのアクセス制限

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,8 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: :edit 
+  before_action :move_to_index, only: :edit
   before_action :move_to_signed_in, only: :new
+
   def index
     @items = Item.all.order('created_at DESC')
   end
@@ -20,7 +23,7 @@ class ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
   end
-
+  
   def edit
     @item = Item.find(params[:id])
   end
@@ -33,6 +36,7 @@ class ItemsController < ApplicationController
       render action: :edit, status: :unprocessable_entity
    end
   end
+
   private
 
   def item_params
@@ -42,7 +46,12 @@ class ItemsController < ApplicationController
 
   def move_to_signed_in
     return if user_signed_in?
-
     redirect_to new_user_session_path
+  end
+  def move_to_index
+    item = Item.find(params[:id])
+    unless item.user_id == current_user.id
+      redirect_to action: :index
+    end
   end
 end
