7e7a63f4a486bfc8d1b0e403645a73d3081cdeac
sinkaigyo46 <suguru.1096@yahoo.ne.jp>
2023-08-02 19:24:01 +0900

    Revert "編集ページへの遷移"
    This reverts commit daac5d078928f84b1fccd9a40b3fcb2e179f77c7.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,18 +21,6 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
-  def edit
-    @item = Item.find(params[:id])
-  end
-
-  def update
-    @item = Item.find(params[:id])
-    if @item.update(item_params)
-      redirect_to item_path
-    else
-      render action: :edit, status: :unprocessable_entity
-   end
-  end
   private
 
   def item_params
