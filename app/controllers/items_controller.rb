820890e851514de339506e629733807e547f3066
sinkaigyo46 <suguru.1096@yahoo.ne.jp>
2023-08-03 14:11:01 +0900

    コード修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :authenticate_user!, only: [:new, :edit, :destroy]
   before_action :move_to_index, only: :edit
   before_action :set_item, only: [:show, :edit, :update, :destroy]
   def index
@@ -34,8 +34,10 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-  @item.destroy
-  redirect_to root_path
+    if @item.user == current_user
+    @item.destroy
+    redirect_to root_path
+    end
   end
 
   private
