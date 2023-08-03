cc3d0b787cc71438d6446383ab8908b3bdb3bc6f
sinkaigyo46 <suguru.1096@yahoo.ne.jp>
2023-08-03 14:36:12 +0900

    コード修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -36,8 +36,8 @@ class ItemsController < ApplicationController
   def destroy
     if @item.user == current_user
     @item.destroy
-    redirect_to root_path
     end
+    redirect_to root_path
   end
 
   private
