e7b2657516945adb2b35e617beaa5200f5e6fac9
sinkaigyo46 <suguru.1096@yahoo.ne.jp>
2023-08-04 14:03:32 +0900

    購入画面の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
-  resources :items
-  resources :shippings
+  resources :items do
+    resources :shippings, only: [:index, :create]
+  end
 end
