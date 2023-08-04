c09bb2c3d5f49e2ab7b1cf4d91469e04c6cfa01d
sinkaigyo46 <suguru.1096@yahoo.ne.jp>
2023-08-04 17:21:56 +0900

    フォームオブジェクトの実装

---
+++ app/models/order.rb
@@ -0,0 +1,19 @@
+class Order
+  include ActiveModel::Model
+   attr_accessor :user, :item, :postal_code, :city_town, :street_number, :building_name, :phone_number, :shipping, :prefecture_id
+  
+   with_options presence: true do
+    validates :item, :user, :city_town, :street_number 
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :phone_number, format: { with: /\A\d{10,11}\z/,message: "is invalid. Include hyphen(-)"}
+   end
+  
+
+  def save
+    user = User.find_by(id: self.user)
+    item = Item.find_by(id: self.item)
+   shipping = Shipping.create(user: user, item: item)
+   ShippingAddress.create(postal_code: postal_code, city_town: city_town, street_number: street_number, building_name: building_name, phone_number: phone_number, prefecture_id: prefecture_id, shipping: shipping)
+  end
+end
