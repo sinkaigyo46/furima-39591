208fcf23b05e0e191ed84b27757f4989633d930b
sinkaigyo46 <suguru.1096@yahoo.ne.jp>
2023-08-05 17:49:27 +0900

    秘密鍵作り直し

--- app/controllers/shippings_controller.rb
+++ app/controllers/shippings_controller.rb
@@ -33,7 +33,7 @@ class ShippingsController < ApplicationController
   end
   
   def pay_item
-  Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
   Payjp::Charge.create(
     amount: @item.price,  # 商品の値段
     card: order_params[:token],    # カードトークン
