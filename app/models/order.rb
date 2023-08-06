3fb8a14866951d224d7db9a433d83aa72a9f1cd2
sinkaigyo46 <suguru.1096@yahoo.ne.jp>
2023-08-06 17:11:24 +0900

    コード修正

--- app/models/order.rb
+++ app/models/order.rb
@@ -11,8 +11,8 @@ class Order
   end
 
   def save
-    shipping = Shipping.create(user_id:, item_id:)
-    ShippingAddress.create(postal_code:, city_town:, street_number:,
-                           building_name:, phone_number:, prefecture_id:, shipping: shipping.id)
+    shipping = Shipping.create(user_id: user_id, item_id: item_id)
+    ShippingAddress.create(postal_code: postal_code, city_town: city_town, street_number: street_number,
+                           building_name: building_name, phone_number: phone_number, prefecture_id: prefecture_id, shipping_id: shipping.id)
   end
 end
